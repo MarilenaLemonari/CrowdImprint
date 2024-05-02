1ded6d3916e2f55cb0a64b19ee23000cf5bdc03a
MarilenaLemonari <marilena.lemonari13@gmail.com>
2024-05-02 12:15:45 +0300

    flock traj

--- Data/data_analysis.py
+++ Data/data_analysis.py
@@ -22,9 +22,20 @@ def preprocess_data(source):
     [frame_number pedestrian_ID pos_x pos_z pos_y v_x v_z v_y ]
     https://icu.ee.ethz.ch/research/datsets.html
     25fps videos BUT 2.5 fps annotations (0.4s timestep)
+  
+  source == "Flock"
+    ./Flock/*.csv 
+    [time; pos_x; pos_y]
+    0.04s timestep => 25fps
   """
 
   if source == "ETH":
+    """
+    returns: list 'agents_traj'
+            list of #eth files i.e., eth_hotel and eth_road,
+            within which a list of #agents,
+            of positional arrays of shape (#frames, 3) with columns: [frame_no, pos_x, pos_y].
+    """
     # traj_path =  "C:/PROJECTS/SocialLandmarks/Data/Trajectories/eth_hotel.txt"
     traj_dir = "C:/PROJECTS/SocialLandmarks/Data/Trajectories"
     all_files = os.listdir(traj_dir)
@@ -73,6 +84,54 @@ def preprocess_data(source):
 
     return agents_traj
 
+  elif source == "Flock":
+    """
+    returns: list 'agents_traj'
+            list of flock scenarios i.e., 1,
+            within which a list of #agents,
+            of positional arrays of shape (#timesteps, 3) with columns: [timestep, pos_x, pos_y]
+    """
+
+    traj_dir = "C:/PROJECTS/SocialLandmarks/Data/Trajectories/Flock/"
+    all_agent_files = os.listdir(traj_dir)
+    column_names = ["Timestep", "pos_x", "pos_y" ]
+
+    agents_traj = []
+    agents_traj_list = []
+    timestep = 0.04
+
+    for agent_id,agent_file in enumerate(all_agent_files):
+      # Load file:
+      agent_traj = pd.read_csv(traj_dir+agent_file, header=None)
+      agent_traj[column_names] = agent_traj[0].str.split(';', expand=True)
+      agent_traj[column_names[0]] = agent_traj[column_names[0]].astype(float) 
+      agent_traj[column_names[1]] = agent_traj[column_names[1]].astype(float)
+      agent_traj[column_names[2]] = agent_traj[column_names[2]].astype(float)
+      agent_traj.drop(0, axis=1, inplace=True)
+      agents_traj_values = agent_traj.values
+      
+      # Cutoff trajectories according to specified framerate/duration:
+      frame_cutoff = int(agents_traj_values[-1,0]/timestep + 1) # TODO: Custom e.g.25
+      start_frame = int(agents_traj_values[0,0]/timestep + 1)
+      cutoff = 0
+      cutoff_list = []
+      start_list = [0]
+      for i in range(1,agents_traj_values.shape[0]):
+        end_frame = int(agents_traj_values[i,0]/timestep + 1)
+        if end_frame-start_frame > frame_cutoff:
+          cutoff += 1
+          cutoff_list.append(i)
+          start_list.append(i)
+          start_frame = end_frame
+      cutoff_list.append(agents_traj_values.shape[0]+1)
+
+      for c in range(cutoff+1):
+        agents_traj_list.append(agents_traj_values[start_list[c]:cutoff_list[c],:])
+      # agents_traj_list.append(agents_traj_values) # *(386,3)
+
+    agents_traj.append(agents_traj_list)
+    return agents_traj
+
   else:
     print("INPUT ERROR: Source Type Not Supported!")
     return
@@ -83,7 +142,7 @@ def visualize_agent_traj(agents_traj, title):
   max_dist = []
   max_width = []
   processed_points = []
-  for i, agent_traj in enumerate(agents_traj[:10]):
+  for i, agent_traj in enumerate(agents_traj[:20]):
     if len(agent_traj) <= 0:
       continue #TODO: pedestrian id 21
     
@@ -146,11 +205,12 @@ def visualize_agent_traj(agents_traj, title):
   # plt.xlim(-abs(int(max_overall/4))-1, abs(int(max_overall/4))+1) #TODO: regulate
   tol_x = 0.1
   plt.xlim(-abs(max_value_w)-tol_x, abs(max_value_w)+tol_x) 
+  plt.savefig(f"{title}")
   plt.show()
 
   return data, [max_value, max_value_h, max_value_w]
 
-def perform_dtw(data, max_list):
+def perform_dtw(data, max_list, n_clusters, degree):
 
   print("perform_dtw()")
 
@@ -197,51 +257,83 @@ def perform_dtw(data, max_list):
   for i,datapoints in enumerate(data):
     datapoints = datapoints.reshape((1,datapoints.shape[0], datapoints.shape[1]))
     padded_data = np.pad(datapoints, ((0, 0), (max_length - n_points[i], 0), (0, 0)), mode='constant')
-    plt.plot(padded_data[0,:,0], padded_data[0,:,1],'lightgrey',linestyle = 'dotted')
+    plt.plot(padded_data[0,:,0], padded_data[0,:,1],'lightgrey',linestyle = 'dotted', label = f'gt_{i}')
     if i == 0:
       data_full = padded_data
     else:
       data_full = np.vstack((data_full, padded_data))
 
-  n_clusters = 3
+  # n_clusters = 5
   kmeans = TimeSeriesKMeans(n_clusters=n_clusters, metric="dtw")
   kmeans.fit(data_full)
 
   centroids = kmeans.cluster_centers_
 
+  # Get amount of data in each cluster
+  cluster_labels = kmeans.labels_
+  unique_labels, counts = np.unique(cluster_labels, return_counts=True)
+  dict = {}
+  for label, count in zip(unique_labels, counts):
+      dict[label] = count
+      print(f"Cluster {label}: {count} examples")
+
+  counter = 0
   for centroid in centroids:
+      num = dict[counter]
+      counter += 1
       x = centroid[:, 0]
       y = centroid[:, 1]
-      plt.plot(x, y,'firebrick', linestyle = 'dashdot')
+      plt.plot(x, y,'firebrick', linestyle = 'dashdot', label = f'centroid_{counter}')
       indices = np.column_stack((0,np.where(x != 0)))
       unique_y = x[indices[0,:]]
       unique_x = y[indices[0,:]]
-      degree = 3
+      # degree = 3
       coefficients = np.polyfit(unique_x,unique_y, degree)
       poly_func = np.poly1d(coefficients)
       # interp_func = interp1d(np.unique(x), np.unique(y), kind = 'linear')
       x_interp = np.linspace(unique_x.min(), unique_x.max(), 1000)
       y_interp = poly_func(x_interp)
       # y_interp = interp_func(x_interp)
-      plt.plot(y_interp,x_interp,'dimgrey', linewidth = 2)
+      plt.plot(y_interp,x_interp,'dimgrey', linewidth = 2, label = f'if_{counter}num_{num}')
 
-  plt.plot(0,0,'orange',marker='.', markersize=10)
-  plt.plot(0,max_value,'orange',marker='.', markersize=10)
+  plt.plot(0,0,'orange',marker='.', markersize=10, label = 'point_spawn')
+  plt.plot(0,max_value,'orange',marker='.', markersize=10, label = 'point_goal')
+
+  for line in plt.gca().lines:
+    line.set_visible(False)
+    if 'point' in line._label:
+      line.set_visible(True)
+
+  for line in plt.gca().lines: 
+    if 'if_' in line._label:
+       line.set_visible(True)
+       if_part, num_part = line._label.split('num_')
+       plt.title(f'No.Trajectories: {num_part}')
+       plt.savefig(f'{if_part}')
+       line.set_visible(False)
+
+  for line in plt.gca().lines:
+    line.set_visible(True)
+
   tol_y = 1.5
-  plt.ylim(-abs(max_value_h)-tol_y, abs(max_value_h)+tol_y)
+  # plt.ylim(-abs(max_value_h)-tol_y, abs(max_value_h)+tol_y) # TODO: regulate
   tol_x = 0.1
-  plt.xlim(-abs(max_value_w)-tol_x, abs(max_value_w)+tol_x) 
+  # plt.xlim(-abs(max_value_w)-tol_x, abs(max_value_w)+tol_x) 
   plt.title('Representative Curves using DTW')
   plt.xlabel('X-axis')
   plt.ylabel('Y-axis')
   #plt.legend(["one","two","three"])
+  plt.savefig('Representative Curves using DTW')
   plt.show()
 
 if __name__ ==  '__main__':
   print("Main")
-  agents_traj = preprocess_data(source = "ETH")
-  data, [max_value, max_value_h, max_value_w] = visualize_agent_traj(agents_traj = agents_traj[0], title = "ETH_Hotel Trajectories")
+  # agents_traj = preprocess_data(source = "ETH")
+  agents_traj = preprocess_data(source = "Flock")
+  # data, [max_value, max_value_h, max_value_w] = visualize_agent_traj(agents_traj = agents_traj[0], title = "ETH_Hotel Trajectories")
   # visualize_agent_traj(agents_traj = agents_traj[1], title = "ETH_Road Trajectories")
-  perform_dtw(data, [max_value, max_value_h, max_value_w])
+  data, [max_value, max_value_h, max_value_w] = visualize_agent_traj(agents_traj = agents_traj[0], title = "Flock Trajectories")
+  exit()
+  perform_dtw(data, [max_value, max_value_h, max_value_w], n_clusters = 5, degree = 3)
 
 
